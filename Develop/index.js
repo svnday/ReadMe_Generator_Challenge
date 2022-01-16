deffb3d1dbb97bec99e3c48e215619ee30808537
svnday <40030806+svnday@users.noreply.github.com>
2022-01-16 11:22:06 -0600

    finalized questions for generating readme

--- Develop/index.js
+++ Develop/index.js
@@ -2,7 +2,57 @@
 const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your Project?',
+    },
+    {
+        type: 'input',
+        name: 'Description',
+        message: 'Please enter a description of your project.',
+    },
+    {
+        type: 'input',
+        name: 'Installation instructions',
+        message: 'Please provide a step-by-step description of how to get the development environment running.',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions and examples for use.',
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'What licenses are being used?'
+    },
+    {
+        type: 'input',
+        name: 'contributors',
+        message: 'Who are the contributors?'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What commands are needed to run tests?'
+    },
+    {
+        type: 'input',
+        name: 'inquiries',
+        message: 'Contact info for inquiries'
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub username?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email?'
+    }
+]
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
